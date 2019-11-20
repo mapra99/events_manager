cf8411f0ed58359e711f2302b71554eccecbd3b1
Miguel Prada <mapra99@gmail.com>
2019-11-20 17:16:37 -0500

    divided the attended events into the previous ones and the upcoming

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   has_many :created_events, foreign_key: :creator_id, class_name: 'Event'
   has_many :attendings, foreign_key: :attendee_id
   has_many :attended_events, through: :attendings, source: :attended_event
+
+  def upcoming_events
+    attended_events.where('date >= ?', Date.today)
+  end
+
+  def previous_events
+    attended_events.where('date < ?', Date.today)
+  end
 end
