e5ce82c3b0b4a22216b7b61640e6c52a7ddb16d8
Miguel Prada <mapra99@gmail.com>
2019-11-20 16:14:30 -0500

    homepage now shows only title, location and date. new event button added in nav

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,7 +10,7 @@ class EventsController < ApplicationController
   def create
     @event = current_user.created_events.build(event_params)
     if @event.save
-      redirect_to root_path
+      redirect_to @event
     else
       render html: 'Error'
     end
