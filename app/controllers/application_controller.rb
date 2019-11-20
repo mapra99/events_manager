52f4366644ce7f646af56349b3630b839ec94672
Miguel Prada <mapra99@gmail.com>
2019-11-20 12:24:20 -0500

    moved the location of authentication helper methods for security

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
   include ApplicationHelper
+
+  def login(user)
+    session[:user_id] = user.id
+    @current_user = user
+  end
+
+  def logout
+    session.delete(:user_id)
+    @current_user = nil
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,7 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def login(user)
-    session[:user_id] = user.id
-    @current_user = user
-  end
-
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
-  end
-
-  def logout
-    session.delete(:user_id)
-    @current_user = nil
   end
 end
