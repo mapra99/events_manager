fdc10df2c802b1751902cd6d7411075ac47eeada
Miguel Prada <mapra99@gmail.com>
2019-11-28 15:00:15 -0500

    test for user to posts association added

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'a created user' do
+    before(:example) do
+      @user = User.create(name: 'This is a user name',
+                          email: 'email@example.com',
+                          password: 'password123',
+                          password_confirmation: 'password123')
+    end
+
+    it 'can create multiple events' do
+      expect {
+        @user.created_events.create(title: 'This is an event title',
+                                    description: 'This is the description of the event',
+                                    date: Date.today,
+                                    location: 'Example City')
+  
+        @user.created_events.create(title: 'This is an event 2 title',
+                                    description: 'This is the description of the event 2',
+                                    date: 2.days.ago,
+                                    location: 'Example City')
+      }.to change {@user.created_events.count}.by(2)
+    end
+  end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,10 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET /users" do
-    it "works! (now write some real specs)" do
-      get users_index_path
-      expect(response).to have_http_status(200)
+  # describe "GET /users" do
+  #   it "works! (now write some real specs)" do
+  #     get users_index_path
+  #     expect(response).to have_http_status(200)
+  #   end
+  # end
+  context 'someone not registered nor logged in' do
+    it 'can signup' do
+    end
+
+    it 'cannot create an event' do
+    end
+
+    it 'cannot attend an event' do
+    end
+
+    it 'can see an event' do
     end
   end
 end
