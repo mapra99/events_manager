4169e6aa3882a4d3b8679c5a2733b78762c7e8f1
Miguel Prada <mapra99@gmail.com>
2019-11-27 20:22:39 -0500

    initialized Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :created_events, foreign_key: :creator_id, class_name: 'Event'
   has_many :attendings, foreign_key: :attendee_id
   has_many :attended_events, through: :attendings, source: :attended_event
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get '/signup', to: 'users#new'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
